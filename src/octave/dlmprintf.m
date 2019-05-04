514f5abf909c706ae58f50d0c59615264d4b0a96
Jan Singon Slany <slanyj@centrum.cz>
2019-05-05 00:31:50 +0200

    Remove debugging output

--- src/octave/dlmprintf.m
+++ src/octave/dlmprintf.m
@@ -10,13 +10,11 @@ function dlmprintf(file, format, M, dlm, H)
 
 	## Build format for header and data
 
-	headerformat = "%s, ";
 	cellformat = strsplit(format);              # Format of individual cells
 	c = length(cellformat);                     # Number of columns
 
 	## Calculate width of each column
 	## To avoid reading all data, consider only the header and first data row
-#	colwidth = [];
 	headerline = "";
 	dataformatline = "";
 	delim = dlm;
@@ -29,22 +27,10 @@ function dlmprintf(file, format, M, dlm, H)
 			delim = "";
 		endif
 		f = sprintf("%%%ds", colwidth);
-		printf("Column format: %s\n", f);
 		headerline = [headerline sprintf(f, headerstr) delim];
 		f = sprintf("%%%ds", colwidth - length(datastr) + length(datafmt));
 		dataformatline = [dataformatline sprintf(f, datafmt) delim];
 	endfor
-	disp("Header:");
-	disp(headerline);
-	disp("Data format:");
-	disp(dataformatline);
-
-#	dataformat = cell(1, 2*c - 1);
-#	dataformat(1:2:2*c-1) = cellformat;
-#	dataformat(2:2:2*c-2) = dlm;
-#	dataformat = strcat(dataformat{:});
-#	disp("Data format:");
-#	disp(dataformat);
 
 	## Write
 	fprintf(file, headerline);

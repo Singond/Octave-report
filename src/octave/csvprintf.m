062d89ea073d2c6431df0a9441b0de4535a795f6
Jan Singon Slany <slanyj@centrum.cz>
2019-05-05 10:55:52 +0200

    Add CSV export function

---
+++ src/octave/csvprintf.m
@@ -0,0 +1,6 @@
+## Export numeric array to CSV file.
+##
+## This is equivalent to calling `dlmprintf(file, format, M, ', ', H)`.
+function csvprintf(file, format, M, H)
+	function dlmprintf(file, format, M, ', ', H);
+endfunction

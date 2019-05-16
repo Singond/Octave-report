38841f7c9331fd255665444e2d2676399ff7f46f
Jan Singon Slany <slanyj@centrum.cz>
2019-05-16 23:46:05 +0200

    Support more types in LaTeX export

--- src/octave/writelatexvars.m
+++ src/octave/writelatexvars.m
@@ -1,3 +1,14 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} writelatexvars(@var{file}, @var{V})
+##
+## Export variables into new LaTeX macro definitions. The variables are
+## written to a standalone @code{.tex} file, which can be @code{input}
+## into another LaTeX document.
+##
+## @var{file} can be a filename or a file handle. If it is a filename, a file
+## with this name is written.
+##
+## @end deftypefn
 function writelatexvars(file, V)
 	% Get file handle
 	if (ischar(file))
@@ -8,10 +19,11 @@ function writelatexvars(file, V)
 		f = file;
 		fprivate = 0;
 	else
-		error(["Bad 'file' argument. Expecting file name or handle, got ", ...
-				typeinfo(file)]);
+		print_usage();
 	endif
 
+#	imagunit = '\mathrm{i}';
+	imagunit = 'i';
 	if (isstruct(V))
 		if (length(V) > 1)
 			error(["The input must be a scalar structure, " ...
@@ -20,13 +32,27 @@ function writelatexvars(file, V)
 		% Input is key-value pairs
 		for [val, name] = V;
 			% TODO Handle different types
-			if (isinteger(val))
-				valstr = sprintf("%d", val);
+			if (isscalar(val))
+				if (isinteger(val))
+					valstr = sprintf("%d", val);
+				elseif(isreal(val))
+					valstr = sprintf("%g", val);
+				elseif(iscomplex(val))
+					valstr = sprintf("%g + %g%s", real(val), imag(val), imagunit);
+				else
+					error("Variable '%s' is of unsupported scalar type (%s)", ...
+						name, typeinfo(val));
+				endif
+			elseif (ischar(val))
+				valstr = val;
 			else
-				valstr = sprintf("%f", val);
+				error("Variable '%s' is of unsupported type (%s)", ...
+						name, typeinfo(val));
 			endif
 			fprintf(f, '\\newcommand\\%s{%s}\n', name, valstr);
 		endfor
+	else
+		error("'V' must be a scalar structure, got %s", typeinfo(V));
 	endif
 
 	% Clean up if the file was created in this function

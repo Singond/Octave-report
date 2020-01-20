004023cca3778fe02be920357eddbab29555fca7
Jan Slany <jan.slany@ais-servis.cz>
2020-01-20 16:16:22 +0100

    Add function header

--- src/octave/rounds.m
+++ src/octave/rounds.m
@@ -1,3 +1,29 @@
+## Copyright (C) 2020 Jan Slany
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {Function file} {@var{r} =} rounds(@var{x}, @var{digits})
+## @deftypefnx {Function file} {@var{r} =} rounds(@var{x})
+## Round @var{x} to the given number of significant digits (@var{digits}).
+## If @var{digits} is not given, round to one significant digit.
+## @end deftypefn
+
+## Author: Jan "Singon" Slany <singond@seznam.cz>
+## Created: January 2020
+## Keywords: round
 function r = rounds(x, digits)
 	if (!isnumeric(x))
 		error("x must be numeric");

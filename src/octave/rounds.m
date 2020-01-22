1a9f87092b48bd45aea0bc9b77bdc29bd06ce111
Jan Slany <jan.slany@ais-servis.cz>
2020-01-22 14:39:42 +0100

    Make rounds work with matrices and negative numbers

--- src/octave/rounds.m
+++ src/octave/rounds.m
@@ -17,8 +17,10 @@
 ## -*- texinfo -*-
 ## @deftypefn  {Function file} {@var{r} =} rounds(@var{x}, @var{digits})
 ## @deftypefnx {Function file} {@var{r} =} rounds(@var{x})
-## Round @var{x} to the given number of significant digits (@var{digits}).
-## If @var{digits} is not given, round to one significant digit.
+## Round @var{x} to the number of significant digits given by @var{digits}.
+##
+## @var{digits} must be either a scalar or a matrix broadcastable to the
+## same shape as @var{x}. If it is omitted, it defaults to 1.
 ## @end deftypefn
 
 ## Author: Jan "Singon" Slany <singond@seznam.cz>
@@ -36,13 +38,14 @@ function r = rounds(x, digits)
 	if (any(mod(digits, 1) != 0))
 		error("'digits' must be integer");
 	endif
+	sgn = sign(x);
+	x = abs(x);                 # Ensure x is positive
 	f = floor(log10(x));        # Order of the first significant digit
 	s = f - digits + 1;         # Order of the last non-zero digit in result
-	if (s != 0)
-		r = round(x.*(10^(-s))) .* 10^s;
-	else
-		r = round(x);
-	endif
+	r = zeros(size(x));
+	M = (s != 0);
+	r(M) = sgn(M) .* round(x(M).*(10.^(-s(M)))) .* 10.^s(M);
+	r(!M) = sgn(!M) .* round(x)(!M);
 endfunction
 
 %!error <x must be numeric> rounds("abc", 1.5);
@@ -60,3 +63,16 @@ endfunction
 %!assert(rounds(pi, 6), 3.14159, eps(3.14159));
 %!assert(rounds(pi, 7), 3.141593, eps(3.141593));
 %!assert(rounds(pi, 8), 3.1415927, eps(3.1415927));
+
+%!assert(rounds(-pi), -3);
+%!assert(rounds(-pi, 1), -3);
+%!assert(rounds(-pi, 2), -3.1, eps(3.1));
+%!assert(rounds(-pi, 3), -3.14, eps(3.14));
+%!assert(rounds(-pi, 4), -3.142, eps(3.142));
+%!assert(rounds(-pi, 5), -3.1416, eps(3.1416));
+%!assert(rounds(-pi, 6), -3.14159, eps(3.14159));
+%!assert(rounds(-pi, 7), -3.141593, eps(3.141593));
+%!assert(rounds(-pi, 8), -3.1415927, eps(3.1415927));
+
+%!assert(rounds((1:3)'.*[pi e], [2 3]), [3.1 2.72; 6.3 5.44; 9.4 8.15], eps(10));
+%!assert(rounds((1:3)'.*[pi e], [1 2 3]'), [3 3; 6.3 5.4; 9.42 8.15], eps(10));

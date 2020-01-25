67c52e5e107c1ba136a57d9806bb58995e3c0d49
Jan Singon Slany <singond@seznam.cz>
2020-01-25 17:44:43 +0100

    Update csvformat documentation

--- src/octave/csvformat.m
+++ src/octave/csvformat.m
@@ -1,21 +1,11 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function File} {} csvformat(@var{file}, @var{format}, @var{M}, @var{h})
-##
-## Export numeric array @var{M} to CSV format with a header @var{H}
-## to @var{file}.
+## Export numeric array @var{M} to comma-separated values format
+## with header @var{h} to @var{file}.
 ##
 ## This is equivalent to calling
 ## @w{@code{dlmformat(file, format, M, ", ", H)}}.
-##
-## @var{file} can be a filename or a file handle. If it is a filename, a file
-## with this name is written.
-##
-## The individual cells are formatted according to @var{format}. This is
-## a space-separated string containing a format string for each column.
-## This argument is split at spaces, a comma and space is inserted
-## between them and a newline is appended to the end. Care must be taken
-## to ensure that the number of elements matches the number of columns in
-## the input matrix @var{M}.
+## @seealso{dlmformat}
 ## @end deftypefn
 function csvformat(file, format, M, H)
 	dlmformat(file, format, M, ', ', H);
